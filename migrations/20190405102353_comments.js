8d91011df5ccb73f0b01b756e9b0b1abfb361a6a
Philip Ji <phil12p345@gmail.com>
2019-04-05 14:21:13 +1300

    Getting there

--- migrations/20190405102353_comments.js
+++ migrations/20190405102353_comments.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable('comments', (table) => {
     table.increments('id').primary()
     table.string('comment_name')
-    table.integer('review').references('reviews.fist_of_five')
+    table.integer('review_id').references('reviews.id')
     table.string('slack_id')
   })
 }
